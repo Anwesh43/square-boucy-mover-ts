8e593072adf42617c75f998bab7644b5f3777a7e
Anwesh43 <anweshthecool0@gmail.com>
2020-01-10 11:04:48 +0530

    adding SSMNode class

--- index.ts
+++ index.ts
@@ -124,3 +124,45 @@ class Animator {
         }
     }
 }
+
+class SSMNode {
+
+    next : SSMNode
+    prev : SSMNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i <  nodes - 1) {
+            this.next = new SSMNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawMSSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SSMNode {
+        var curr : SSMNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
