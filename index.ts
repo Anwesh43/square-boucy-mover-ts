dbb6245e4f1c3acca393809beae76324560cb05c
Anwesh43 <anweshthecool0@gmail.com>
2020-01-10 10:51:15 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -22,3 +22,30 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class DrawingUtil {
+
+    static drawMovingSquare(context : CanvasRenderingContext2D, i : number, w : number, scale : number) {
+        const gap : number = w / tracks
+        const sf : number = ScaleUtil.sinify(scale)
+        const sfi : number = ScaleUtil.divideScale(sf, i, tracks)
+        const sfi1 : number = ScaleUtil.divideScale(sfi, 0, 2)
+        const sfi2 : number = ScaleUtil.divideScale(sfi, 1, 2)
+        context.save()
+        context.translate(i * gap, -gap)
+        context.fillRect(gap * sfi2, 0, gap * sfi1 - gap * sfi1, gap)
+        context.restore()
+    }
+
+    static drawMSSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.fillStyle = foreColor
+        const gap : number = h / (nodes + 1)
+        context.save()
+        context.translate(0, gap * (i + 1))
+        const scDiv : number = scale / tracks
+        DrawingUtil.drawMovingSquare(context, Math.floor(scale / scDiv), w, scale)
+        context.restore()
+    }
+}
